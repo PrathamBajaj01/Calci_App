568676918264761782702f7c8757b4f990060599
Pratham <prathamredwood@gmail.com>
2025-11-21 13:20:49 +0530

    one more test added

--- test_calci.py
+++ test_calci.py
@@ -8,6 +8,7 @@ def test_add():
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(10, 4) == 6
+    assert subtract(3, 5) == -2
 
 def test_multiply():
     assert multiply(2, 3) == 6
